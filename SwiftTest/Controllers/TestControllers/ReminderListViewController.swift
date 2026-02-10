96d58af20fa1fa332e00201e37d30b6f460c1ff1
zhangliang <zhangliangmac@163.com>
2026-02-10 14:34:38 +0800

    修改yml

--- SwiftTest/Controllers/TestControllers/ReminderListViewController.swift
+++ SwiftTest/Controllers/TestControllers/ReminderListViewController.swift
@@ -201,7 +201,7 @@ extension Array where Element == Reminder {
     }
 }
 
-#if DEBUG
+//#if DEBUG
 extension Reminder {
     static var sampleData = [
         Reminder(title: "Submit reimbursement report", dueDate: Date().addingTimeInterval(800.0), notes: "Don't forget about taxi receipts"),
@@ -215,7 +215,7 @@ extension Reminder {
         Reminder(title: "Add beta testers to TestFlight", dueDate: Date().addingTimeInterval(101000.0),  notes: "v0.9 out on Friday")
     ]
 }
-#endif
+//#endif
 
 @available(iOS 15.0, *)
 extension Date {
